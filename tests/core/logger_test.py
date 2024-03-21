c044e2b2ffdf407ac6d3387af4d214b929d2ce6a
Ross Tsenov <tsnovronia3@gmail.com>
2024-03-21 01:50:38 +0000

    Initialize library

--- tests/core/logger_test.py
+++
@@ -1,7 +0,0 @@
-def test_logger_logging() -> None:
-    """Test if the logger correctly logs a message."""
-    from src.logger import logger
-
-    test_message = "This is a test message"
-    logger.info(test_message)
-    logger.setLevel("DEBUG")
