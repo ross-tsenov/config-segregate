6f90c09fb6c5f76fa3f87b0c6bd3efe45055a06a
Ross Tsenov <ross.tsenov@gmail.com>
2024-03-31 20:42:06 +0000

    Fix nested logic, adn remove complicated parts for now

--- config_cascade/core.py
+++ config_cascade/core.py
@@ -6,7 +6,7 @@ from .readers import read_file
 
 __all__ = [
     "load_config",
-    "load_cascaded_configs",
+    "load_segregated_configs",
     "load_base_config",
 ]
 
@@ -14,7 +14,6 @@ __all__ = [
 PATH_PREFIX = r"${{"
 PATH_SUFFIX = r"}}"
 BASE_CONFIG_KEY = r"__base__"
-REMOVE_KEY = r"__remove__"
 
 
 def update_nested_dict(data: Dict[str, Any], updates: Any) -> Dict[str, Any]:
@@ -24,18 +23,15 @@ def update_nested_dict(data: Dict[str, Any], updates: Any) -> Dict[str, Any]:
     for key, value in updates.items():
         current_value = data.get(key)
 
-        if isinstance(value, str) and value == REMOVE_KEY:
-            data.pop(key)
-        elif isinstance(current_value, dict) or current_value is None:
-            nested_dict = current_value if current_value is not None else dict()
-            data[key] = update_nested_dict(nested_dict, value)
+        if isinstance(current_value, dict):
+            data[key] = update_nested_dict(current_value, value)
         else:
             data[key] = value
 
     return data
 
 
-def load_cascaded_configs(data: Dict[str, Any]) -> Dict[str, Any]:
+def load_segregated_configs(data: Dict[str, Any]) -> Dict[str, Any]:
     for key, value in data.items():
         if isinstance(value, str) and value.startswith(PATH_PREFIX) and value.endswith(PATH_SUFFIX):
             trimmed_path = value.removeprefix(PATH_PREFIX).removesuffix(PATH_SUFFIX).strip()
@@ -43,29 +39,27 @@ def load_cascaded_configs(data: Dict[str, Any]) -> Dict[str, Any]:
             data[key] = value
 
         if isinstance(value, dict):
-            data[key] = load_cascaded_configs(value)
+            data[key] = load_segregated_configs(value)
 
     return data
 
 
-def load_base_config(data: Dict[str, Any], do_nested_update: bool = True) -> Dict[str, Any]:
-    if BASE_CONFIG_KEY not in data:
-        return data
+def load_base_config(data: Dict[str, Any]) -> Dict[str, Any]:
+    for key, value in data.items():
+        if isinstance(value, dict):
+            data[key] = load_base_config(value)
 
-    base_data = read_file(data[BASE_CONFIG_KEY])
-    base_data = load_cascaded_configs(base_data)
+    base_data = data.pop(BASE_CONFIG_KEY, None)
 
-    if do_nested_update:
-        base_data = update_nested_dict(base_data, data)
-    else:
-        base_data.update(data)
+    if base_data is not None:
+        data = update_nested_dict(base_data, data)
 
-    return base_data
+    return data
 
 
-def load_config(path_to_file: Union[str, PathLike[str], Path], do_nested_update: bool = True) -> Dict[str, Any]:
+def load_config(path_to_file: Union[str, PathLike[str], Path]) -> Dict[str, Any]:
     data = read_file(path_to_file)
-    data = load_cascaded_configs(data)
-    data = load_base_config(data, do_nested_update)
+    data = load_segregated_configs(data)
+    data = load_base_config(data)
 
     return data
