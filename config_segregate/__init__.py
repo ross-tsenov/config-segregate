c5a8f5fa483cdeb22c5c6b54732bebb8bc6c7756
Ross Tsenov <ross.tsenov@gmail.com>
2024-08-19 21:00:00 +0000

    Fix library version issue.

--- config_segregate/__init__.py
+++ config_segregate/__init__.py
@@ -1,18 +1,10 @@
 from .core import *
 from .readers import *
 from .writers import *
+from importlib.metadata import version, PackageNotFoundError
 
 
-def get_version_from_pyproject(filename: str) -> str:
-    """Loads version from .toml file."""
-    with open(filename) as file:
-        for line in file:
-            if not line.strip().startswith("version"):
-                continue
-            version = line.split("=")[1].strip().strip('"').strip("'")
-            return version
-
-    return ""
-
-
-__version__ = get_version_from_pyproject("./pyproject.toml")
+try:
+    __version__ = version("config_segregate")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
